ba254509cba402ea76b56eb981454d401dfb3119
Nate Gowins <ngowins@gmail.com>
2022-04-06 21:57:05 -0500

    React intro exercise

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>This is my first React App! </h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>I'm sure React is amazing, but it sure is confusing! For now :)</p>
         <a
           className="App-link"
           href="https://reactjs.org"
